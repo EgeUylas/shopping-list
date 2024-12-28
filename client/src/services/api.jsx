1a7a707b9037f8a9c0f5ea777a528ab86683b582
Ege Uylas <egeuylas19013@gmail.com>
2024-12-28 18:18:50 +0300

    Update API URL

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://shopping-list-backend-hsjl.onrender.com/api';
 
 // Kullanıcı kaydetme
 export const registerUser = async (name, surname, email, password) => {
